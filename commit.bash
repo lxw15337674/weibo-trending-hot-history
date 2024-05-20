a533ce28312b727c879da534797ced54f96ab321
bhwa233 <404174262@qq.com>
2024-05-20 22:17:29 +0800

    Update commit.bash script to allow empty commits

--- commit.bash
+++ commit.bash
@@ -4,4 +4,4 @@ git config --local user.name "github-actions[bot]"
 git add .
 
 # Commit changes.
-git commit -m "实时更新微博热搜 - $(date +'%Y/%m/%d %I%p')"
+git commit --allow-empty -m "实时更新微博热搜 - $(date +'%Y/%m/%d %I%p')"
