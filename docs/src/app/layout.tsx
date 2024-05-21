a94909ab3035e44b16458a1226b63e31fffdc9a6
Bhwa233 <404174262@qq.com>
2024-05-21 14:43:06 +0800

    chore: Remove ModeToggle component from layout.tsx

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from '@/components/ModeToggle';
 import { SiteHeader } from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
